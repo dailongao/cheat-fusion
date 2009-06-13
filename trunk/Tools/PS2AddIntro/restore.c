6e1e412429e3fe7d4ec866d72ddfea5e9d52d939
forbbsneo <forbbsneo@1e91e87a-c719-11dd-86c2-0f8696b7b6f9>
2009-06-13 08:47:05 +0000

    addintro的一些改动

--- trunk/Tools/PS2AddIntro/restore.c
+++ trunk/Tools/PS2AddIntro/restore.c
@@ -1,12 +1,13 @@
 extern char elftext_ucl_data[];
 extern char text_asm_end[];
+extern char _start_bss[];
 extern char _end_bss[];
-extern int _heap_size;
 void UnpackText(void *ram_ucl_data, void *unpack_dst, unsigned int entry);
 
 void RestoreProcess()
 {
-	memcpy(_end_bss+HEAPSIZE,elftext_ucl_data,text_asm_end-elftext_ucl_data);
-	UnpackText(_end_bss+HEAPSIZE,(void*)0x100000,0x100000);
+	memset(_start_bss,0,(_end_bss-_start_bss)+HEAPSIZE);
+	memcpy(_start_bss,elftext_ucl_data,text_asm_end-elftext_ucl_data);
+	UnpackText(_start_bss,(void*)0x100000,0x100000);
 }
 
