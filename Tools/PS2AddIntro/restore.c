82f20e70b65f2d49f3d6ee99084da9c355abc3e2
forbbsneo <forbbsneo@1e91e87a-c719-11dd-86c2-0f8696b7b6f9>
2010-10-24 08:37:20 +0000

    增加slps解压后，动态修改数据函数

--- Tools/PS2AddIntro/restore.c
+++ Tools/PS2AddIntro/restore.c
@@ -2,7 +2,9 @@ extern char elftext_ucl_data[];
 extern char text_asm_end[];
 extern char _start_bss[];
 extern char _end_bss[];
-void UnpackText(void *ram_ucl_data, void *unpack_dst, unsigned int entry);
+
+void PatchExe(unsigned int* args) __attribute__ ((section (".copy2ram_text")));
+extern void UnpackText(void *ram_ucl_data, void *unpack_dst, unsigned int entry) __attribute__ ((section (".copy2ram_text")));
 
 void RestoreProcess()
 {
@@ -11,3 +13,7 @@ void RestoreProcess()
 	UnpackText(_start_bss,(void*)TEXTSTARTADR,ELFENTRY);
 }
 
+void PatchExe(unsigned int* args)
+{
+	return;
+}
